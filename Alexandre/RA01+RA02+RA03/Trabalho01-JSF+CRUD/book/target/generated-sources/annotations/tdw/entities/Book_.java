2f455bd8dcd6358237d3426511988133d1a7105c
Alexandre Sobota <alexandre.sobota@hotmail.com>
2021-04-05 20:43:22 -0300

    redoing the crud in another environment

--- Alexandre/RA01+RA02+RA03/Trabalho01-JSF+CRUD/book/target/generated-sources/annotations/tdw/entities/Book_.java
+++ Alexandre/RA01+RA02+RA03/Trabalho01-JSF+CRUD/book/target/generated-sources/annotations/tdw/entities/Book_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-04-04T16:58:47")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-04-05T20:37:50")
 @StaticMetamodel(Book.class)
 public class Book_ { 
 
