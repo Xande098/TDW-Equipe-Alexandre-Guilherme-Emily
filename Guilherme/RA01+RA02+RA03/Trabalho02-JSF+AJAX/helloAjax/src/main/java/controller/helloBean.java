38a12392557c86fc126426e30f66ce977bec9ba6
guikoblitz <guilhermerk_contato@hotmail.com>
2021-06-06 20:28:50 -0300

    JSF+AJAX final version

--- Guilherme/RA01+RA02+RA03/Trabalho02-JSF+AJAX/helloAjax/src/main/java/controller/helloBean.java
+++ Guilherme/RA01+RA02+RA03/Trabalho02-JSF+AJAX/helloAjax/src/main/java/controller/helloBean.java
@@ -8,6 +8,7 @@ package controller;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  *
@@ -18,8 +19,26 @@ import java.io.Serializable;
 public class helloBean implements Serializable {
 
     private String name;
+    private String program;
+    private Date someDate;
 
     public helloBean() {
+    }
+    
+    public String getProgram() {
+        return program;
+    }
+    
+    public void setProgram(String program) {
+        this.program = program;
+    }
+    
+    public Date getSomeDate() {
+        return someDate;
+    }
+    
+    public void setSomeDate(Date someDate) {
+        this.someDate = someDate;
     }
 
     public String getName() {
@@ -36,6 +55,28 @@ public class helloBean implements Serializable {
             return "";
         } else {
             return "Ajax message : Welcome " + name;
+        }
+    }
+    
+    public String getSayProgram() {
+        String outStr = "";
+        //check if null?
+        if ("".equals(program) || program == null) {
+            return "";
+        } else {
+            int num = Integer.parseInt(program);
+            switch (num) {
+                case 1:
+                    outStr = "Programa: Tecnologias para Desenvolvimento Web";
+                    break;
+                case 2:
+                    outStr = "Programa: Oficina Certificadora";
+                    break;
+                case 3:
+                    outStr = "Programa: Outra Disciplina";
+                    break;
+            }
+            return outStr;
         }
     }
 
